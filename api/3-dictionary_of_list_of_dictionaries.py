1606f2138ecc4482e20a60a552a1c027fc19640a
m-mwangi <m.mwangi2@alustudent.com>
2023-11-01 13:16:48 +0000

    3-dictionary_of_list_of_dictionaries.py

--- api/3-dictionary_of_list_of_dictionaries.py
+++ api/3-dictionary_of_list_of_dictionaries.py
@@ -1,32 +1,28 @@
 #!/usr/bin/python3
-import requests
-import json
+"""Exports data in the JSON format"""
 
 if __name__ == "__main__":
-    url = "https://jsonplaceholder.typicode.com/todos"
-    response = requests.get(url)
-    tasks = response.json()
 
-    # Create a dictionary to store tasks for each user
-    user_tasks = {}
-    for task in tasks:
-        user_id = task["userId"]
-        task_info = {
-            "username": task["title"],
-            "task": task["title"],
-            "completed": task["completed"]
-        }
+    import json
+    import requests
+    import sys
 
-        # If user_id already exists in the dictionary, append the task, else create a new user entry
-        if user_id in user_tasks:
-            user_tasks[user_id].append(task_info)
-        else:
-            user_tasks[user_id] = [task_info]
+    users = requests.get("https://jsonplaceholder.typicode.com/users")
+    users = users.json()
+    todos = requests.get('https://jsonplaceholder.typicode.com/todos')
+    todos = todos.json()
+    todoAll = {}
 
-    # Convert the dictionary to JSON format
-    json_data = json.dumps(user_tasks, indent=4)
+    for user in users:
+        taskList = []
+        for task in todos:
+            if task.get('userId') == user.get('id'):
+                taskDict = {"username": user.get('username'),
+                            "task": task.get('title'),
+                            "completed": task.get('completed')}
+                taskList.append(taskDict)
+        todoAll[user.get('id')] = taskList
 
-    # Write JSON data to a file
-    with open("todo_all_employees.json", "w") as outfile:
-        outfile.write(json_data)
+    with open('todo_all_employees.json', mode='w') as f:
+        json.dump(todoAll, f)
 
