1123bd72f86ca871cafe4d049f881c6ad7faca1c
m-mwangi <m.mwangi2@alustudent.com>
2023-11-01 13:37:37 +0000

    3-dictionary_of_list_of_dictionaries.py

--- api/3-dictionary_of_list_of_dictionaries.py
+++ api/3-dictionary_of_list_of_dictionaries.py
@@ -1,42 +1,25 @@
 #!/usr/bin/python3
+"""
+    python script that exports data in the JSON format
+"""
 import json
 import requests
-import sys
 
 if __name__ == "__main__":
-    if len(sys.argv) != 2:
-        print("Usage: python3 script.py <employee_id>")
-        sys.exit(1)
+    url = "https://jsonplaceholder.typicode.com/"
+    users = requests.get(url + "users").json()
+    """
+        export to JSON
+    """
 
-    employee_id = sys.argv[1]
-    base_url = "https://jsonplaceholder.typicode.com"
-    user_url = f"{base_url}/users/{employee_id}"
-    tasks_url = f"{base_url}/todos?userId={employee_id}"
-
-    try:
-        user_response = requests.get(user_url)
-        tasks_response = requests.get(tasks_url)
-
-        user_data = user_response.json()
-        tasks_data = tasks_response.json()
-
-        username = user_data["username"]
-
-        task_list = []
-        for task in tasks_data:
-            task_dict = {
-                "username": username,
-                "task": task["title"],
-                "completed": task["completed"]
-            }
-            task_list.append(task_dict)
-
-        result = {employee_id: task_list}
-
-        with open("todo_all_employees.json", "w") as json_file:
-            json.dump(result, json_file)
-
-    except requests.exceptions.RequestException as e:
-        print(f"Error: {e}")
+    with open("todo_all_employees.json", "w") as jsonfile:
+        json.dump({
+            u.get("id"): [{
+                "task": t.get("title"),
+                "completed": t.get("completed"),
+                "username": u.get("username")
+            } for t in requests.get(url + "todos",
+                                    params={"userId": u.get("id")}).json()]
+            for u in users}, jsonfile)
 
 
