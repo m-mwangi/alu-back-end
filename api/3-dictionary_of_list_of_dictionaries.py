e48a303fa7850513c38d784687a342f75fab94a8
m-mwangi <127857750+m-mwangi@users.noreply.github.com>
2023-11-01 15:48:23 +0200

    Update 3-dictionary_of_list_of_dictionaries.py

--- api/3-dictionary_of_list_of_dictionaries.py
+++ api/3-dictionary_of_list_of_dictionaries.py
@@ -1,25 +1,46 @@
 #!/usr/bin/python3
 """
-    python script that exports data in the JSON format
+Export data to JSON format
 """
 import json
 import requests
+import sys
 
 if __name__ == "__main__":
-    url = "https://jsonplaceholder.typicode.com/"
-    users = requests.get(url + "users").json()
-    """
-        export to JSON
-    """
+    if len(sys.argv) != 1:
+        print("Usage: python3 3-dictionary_of_list_of_dictionaries.py")
+        sys.exit(1)
 
-    with open("todo_all_employees.json", "w") as jsonfile:
-        json.dump({
-            u.get("id"): [{
-                "task": t.get("title"),
-                "completed": t.get("completed"),
-                "username": u.get("username")
-            } for t in requests.get(url + "todos",
-                                    params={"userId": u.get("id")}).json()]
-            for u in users}, jsonfile)
+    base_url = "https://jsonplaceholder.typicode.com"
+    users_url = f"{base_url}/users"
 
+    try:
+        user_response = requests.get(users_url)
+        users_data = user_response.json()
 
+        data_to_export = {}
+
+        for user in users_data:
+            user_id = user["id"]
+            username = user["username"]
+
+            tasks_url = f"{base_url}/todos?userId={user_id}"
+            tasks_response = requests.get(tasks_url)
+            tasks_data = tasks_response.json()
+
+            tasks_list = []
+            for task in tasks_data:
+                task_dict = {
+                    "username": username,
+                    "task": task["title"],
+                    "completed": task["completed"]
+                }
+                tasks_list.append(task_dict)
+
+            data_to_export[str(user_id)] = tasks_list
+
+        with open("todo_all_employees.json", "w") as json_file:
+            json.dump(data_to_export, json_file)
+
+    except requests.exceptions.RequestException as e:
+        print(f"Error: {e}")
